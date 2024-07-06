c4f1586ae8b8babf1ed4ea69564059056b11125d
omer chovel <omer.ch.1999@gmail.com>
2024-07-06 17:02:56 +0300

    update

--- frontEnd/src/Components/Tables/GenericTable.jsx
+++ frontEnd/src/Components/Tables/GenericTable.jsx
@@ -149,7 +149,7 @@ export default function GenericTable({
             }
             return { ...item };
           });
-          console.log(newData);
+
           return newData;
         });
       },
